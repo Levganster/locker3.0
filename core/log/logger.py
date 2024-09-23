3c350b2c3b97663b0b7c1e5310a5a8807bdfeccb
Levganster <kuzmitch73son@gmail.com>
2024-09-23 14:24:32 +0300

    added delete log function

--- core/log/logger.py
+++ core/log/logger.py
@@ -1,5 +1,6 @@
 import logging
 import json
+from datetime import datetime
 
 class JSONFormatter(logging.Formatter):
     def format(self, record):
@@ -10,6 +11,10 @@ class JSONFormatter(logging.Formatter):
             'username': record.username  # По умолчанию username = None (null в JSON)
         }
         return json.dumps(log_record)
+    
+    def formatTime(self, record, datefmt=None):
+        # Форматируем время без миллисекунд
+        return datetime.fromtimestamp(record.created).strftime('%Y-%m-%d %H:%M:%S')
 
 # Настройка логирования
 logger = logging.getLogger("jsonLogger")
@@ -55,3 +60,21 @@ def get_logs_as_json(file_path='log.json'):
         return {"error": "Log file not found"}
     except json.JSONDecodeError:
         return {"error": "Error decoding log file"}
+    
+def delete_logs(n, file_path='log.json'):
+    try:
+        with open(file_path, 'r') as log_file:
+            logs = log_file.readlines()
+        
+        # Оставляем только те записи, которые остаются после удаления n первых
+        remaining_logs = logs[n:]
+
+        # Перезаписываем файл с оставшимися логами
+        with open(file_path, 'w') as log_file:
+            log_file.writelines(remaining_logs)
+        
+        print(f"Deleted {n} log(s).")
+    except FileNotFoundError:
+        print("Log file not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
--- core/log/views.py
+++ core/log/views.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException,Security
 from fastapi_jwt import JwtAuthorizationCredentials
 from core.auth.views import access_security
-from core.log.logger import get_logs_as_json
+from core.log.logger import get_logs_as_json, delete_logs
 
 from core.log.config import (
     PREFIX,
@@ -20,3 +20,9 @@ async def get_logs(credentials: JwtAuthorizationCredentials = Security(access_se
     if not credentials["admin"]:
         raise HTTPException(status_code=403, detail="You dont have permission to access")
     return get_logs_as_json()
+
+@router.post('/delete_logs')
+async def delete_logs( amount: int, credentials: JwtAuthorizationCredentials = Security(access_security)):
+    if not credentials["admin"]:
+        raise HTTPException(status_code=403, detail="You dont have permission to access")
+    delete_logs(amount)
