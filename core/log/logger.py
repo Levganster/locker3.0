e0610a8fe1f1b6237f47bf23027dc01ab79f17fd
Levganster <kuzmitch73son@gmail.com>
2024-09-23 14:49:24 +0300

    reversed logs

--- core/log/logger.py
+++ core/log/logger.py
@@ -54,7 +54,7 @@ def get_logs_as_json(file_path='log.json'):
             # Читаем содержимое файла построчно
             logs = log_file.readlines()
             # Парсим каждую строку как JSON
-            logs_json = [json.loads(log.strip()) for log in logs]
+            logs_json = [json.loads(log.strip()) for log in logs][::-1]
             return logs_json
     except FileNotFoundError:
         return {"error": "Log file not found"}
