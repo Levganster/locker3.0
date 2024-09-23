0ed6a7ca1c089bde7e7f3d222741482bbe8eaa72
Levganster <kuzmitch73son@gmail.com>
2024-09-23 14:07:04 +0300

    added log router to main

--- core/main.py
+++ core/main.py
@@ -15,6 +15,7 @@ from core.user.views import router as user_router
 from core.auth.views import router as auth_router
 from core.websockets.views import router as web_router
 from core.control.views import router as control_router
+from core.log.views import router as log_router
 
 # import base settings
 from core.settings import (
@@ -81,6 +82,6 @@ async def redoc_html():
 # add routers to app
 # list of routers to include in app
 # e.g. routers = [my_app_router, users_router, auth_router]
-routers = [auth_router, user_router, web_router, control_router]
+routers = [auth_router, user_router, web_router, control_router, log_router]
 for router in routers:
     app.include_router(router)
