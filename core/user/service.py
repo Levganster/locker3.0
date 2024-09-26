8a4a9dcdfd8c2aa7e1d024a6e7973ef7fb6ab6c1
Levganster <kuzmitch73son@gmail.com>
2024-09-26 10:57:29 +0300

    132132132

--- core/user/service.py
+++ core/user/service.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import IntegrityError
 from core.abstract_repository import SQLAlchemyRepository, AbstractRepository
 from core.user.models import User
 from core.exceptions import ConflictException, NotFoundException
-from core.user.schemas import UserCreateSchema
+from core.user.schemas import UserCreateSchema, UserGetSchema
 
 # TODO: define your service here and rename if it's necessary
 # WARNING: if this sevice doesn't work with DB,
@@ -40,7 +40,7 @@ class UserService:
             return item
         raise NotFoundException()
 
-    async def update(self, id: int, item: UserCreateSchema):
+    async def update(self, id: int, item: UserGetSchema):
         item_dict = item.model_dump()
     
         if upd_item := await self.repository.update_by_id(id, item_dict):
