2e713a311f157db87d8673da0d99c509708b3a61
Levganster <kuzmitch73son@gmail.com>
2024-10-21 14:03:28 +0300

    patch 3.0

--- core/auth/views.py
+++ core/auth/views.py
@@ -14,8 +14,6 @@ from core.user.models import User
 
 access_security = JwtAccessBearerCookie(secret_key="secret_key", auto_error=True, access_expires_delta=timedelta(days=7))
 
-from core.control.views import get_authstate
-
 from core.auth.config import (
     PREFIX,
     TAGS,
@@ -30,15 +28,16 @@ router = APIRouter(
 
 @router.post("/token")
 async def auth(user: AuthCreateSchema, session: AsyncSession = Depends(get_async_session)):
+    from core.control.views import get_authstate
+    
     result = await session.execute(select(User).where(User.username == user.username))
     db_user = result.scalars().first()
 
-    if not db_user:
-        raise HTTPException(status_code=422, detail="Invalid username or password")
-
     if not get_authstate() and not db_user.admin:
         raise HTTPException(status_code=400, detail="Cant auth now")
-    
+
+    if not db_user:
+        raise HTTPException(status_code=422, detail="Invalid username or password")
     if user.password != db_user.password:
         raise HTTPException(status_code=422, detail="Invalid username or password")
     subject = {'username': db_user.username, 'admin': db_user.admin, 'group': db_user.group}
@@ -46,16 +45,16 @@ async def auth(user: AuthCreateSchema, session: AsyncSession = Depends(get_async
 
 @router.post("/cookie")
 async def auth(response: Response, user: AuthCreateSchema, session: AsyncSession = Depends(get_async_session)):
-
+    from core.control.views import get_authstate
 
     result = await session.execute(select(User).where(User.username == user.username))
     db_user = result.scalars().first()
 
-    if not db_user:
-        raise HTTPException(status_code=422, detail="Invalid username or password")
-
     if not get_authstate() and not db_user.admin:
         raise HTTPException(status_code=400, detail="Cant auth now")
+
+    if not db_user:
+        raise HTTPException(status_code=422, detail="Invalid username or password")
 
     if user.password != db_user.password:
         raise HTTPException(status_code=422, detail="Invalid username or password")
